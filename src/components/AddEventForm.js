fdb0d41fe44ed3249f7fadf99168bb4a57e1d7fc
ezl <ericzliu@gmail.com>
2017-09-16 14:37:49 -0400

    Connect form, part 1

--- src/components/AddEventForm.js
+++ src/components/AddEventForm.js
@@ -2,14 +2,31 @@ import React, { Component } from 'react';
 import './AddEventForm.css';
 
 class AddEventForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: '',
+            happiness: '',
+            event: '',
+            notes: ''
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit() {
+        var values = this.state.age + this.state.notes;
+        console.log("handleSubmit", values);
+    }
+
     render() {
         return (
             <div id='addEventForm'>
-                <form>
-                    <input type="text" placeholder="Age" />
-                    <input type="text" placeholder="#" />
-                    <input type="text" placeholder="Event" />
-                    <input type="text" placeholder="Notes" />
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder="Age" value={this.state.age} />
+                    <input type="text" placeholder="#" value={this.state.happiness} />
+                    <input type="text" placeholder="Event" value={this.state.event} />
+                    <input type="text" placeholder="Notes" value={this.state.notes} />
                     <input type="submit" value="+" />
                 </form>
             </div>
